30a18c79d7c5961eae71edb50cc87867b6887171
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-05-28 20:51:32 -0300

    add. project name

--- src/main/java/com/project/bootcamp/controller/StockController.java
+++ src/main/java/com/project/bootcamp/controller/StockController.java
@@ -12,6 +12,7 @@ import com.project.bootcamp.service.StockService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //EndPoint
+@CrossOrigin //used to connect with FrontEnd
 @RestController
 @RequestMapping(value = "/stock") //controller only knows DTO classes, never entity classes
 public class StockController {
